c0bb6ed22c4af8167f89001f903892658d25435c
Esonmurodov <esonmurodov.com@gnail.com>
2021-07-12 05:11:26 -0700

    Add CustomerService

---
+++ src/main/java/com/example/invoice/web/rest/DetailResource.java
@@ -0,0 +1,10 @@
+package com.example.invoice.web.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST controller for managing Detail.
+ */
+@RestController
+public class DetailResource {
+}
