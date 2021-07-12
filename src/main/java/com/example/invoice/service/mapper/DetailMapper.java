97ef004be30bccaed3e426ac74d3d74711900b40
Esonmurodov <esonmurodov.com@gnail.com>
2021-07-12 04:32:26 -0700

    Add mappers

---
+++ src/main/java/com/example/invoice/service/mapper/DetailMapper.java
@@ -0,0 +1,27 @@
+package com.example.invoice.service.mapper;
+
+import com.example.invoice.model.Detail;
+import com.example.invoice.service.dto.DetailDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {})
+public interface DetailMapper extends EntityMapper<DetailDTO, Detail> {
+
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "order.id", target = "orderId")
+    DetailDTO toDto(Detail detail);
+
+    @Mapping(source = "productId", target = "product")
+    @Mapping(source = "orderId", target = "order")
+    Detail toEntity(DetailDTO detailDTO);
+
+    default Detail fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Detail detail = new Detail();
+        detail.setId(id);
+        return detail;
+    }
+}
